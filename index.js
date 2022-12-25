b9a6386762863befb33ce46b5dcc847c17775e0d
awesome <billycrow268@gmail.com>
2022-12-25 11:14:44 -0800

    mongodb cloud atlas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express()
+const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Controller = require("./controller");
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect('mongodb://127.0.0.1:27017/PythonGame', {
+      const conn = await mongoose.connect('mongodb+srv://rrr:kfuture@cluster0.em9vgx2.mongodb.net/PythonGame', {
         useUnifiedTopology: true,
         useNewUrlParser: true,
       });
@@ -26,6 +26,7 @@ const connectDB = async () => {
 connectDB()
 
 app.get("/check", Controller.check);
+app.get("/", (req, res) => {res.send("dddd")});
 app.post("/update", Controller.update);
 
 app.listen(port, () => {
